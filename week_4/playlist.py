ce1cc81f74f882c374f3c96ee338159b387950bb
bgroveben <bgroveben@gmail.com>
2017-03-29 16:10:35 -0700

    Writing Classes That Interact: Class Song

---
+++ week_4/playlist.py
@@ -0,0 +1,3 @@
+# Class Playlist is a type that we are creating to interact with class Song.
+# Initially, the Playlist objet will have a title, but will not contain any songs.
+# The constructor will only take as input the name of the Playlist and create an empty list for the songs.
