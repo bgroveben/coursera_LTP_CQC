d6b589ecbea7394bc61c4a7a02cc1b5266b2aff8
bgroveben <bgroveben@gmail.com>
2017-03-29 15:16:45 -0700

    Writing Special Method __str__()

--- week_4/completed_cash_register.py
+++ week_4/completed_cash_register.py
@@ -1,15 +1,25 @@
-# Add equality method to our CashRegister class.
-
 class CashRegister:
     """
-    A cash register.
+    A cash register with Canadian currency.
     """
 
     def __init__(self, loonies, toonies, fives, tens, twenties):
         """
         (CashRegister, int, int, int, int, int) -> NoneType
 
-        A CashRegister with loonies, toonies, fives, tens, and twenties.
+        A CashRegister object with loonies, toonies, fives, tens, and twenties.
+
+        >>> register = CashRegister(5, 5, 5, 5, 5)
+        >>> register.loonies
+        5
+        >>> register.toonies
+        5
+        >>> register.fives
+        5
+        >>> register.tens
+        5
+        >>> register.twenties
+        5
         """
         self.loonies = loonies
         self.toonies = toonies
@@ -18,11 +28,20 @@ class CashRegister:
         self.twenties = twenties
 
 
-    # If we use the equality operator (==) without defining an __eq__() method for our class, then the __eq__() method
-    # that is inherited from Python's object class (the base class that all other classes inherit from) is used.
-    # Python's object.__eq__() method compares memory addresses, so that 2 different CashRegister instances with
-    # equivalent amounts (like reg1 and reg2 in the doctest below) will evaluate to False instead of True.
-    # Comment out the __eq__() method below and compare the file output.
+    def __str__(self):
+        """
+        (CashRegister) -> str
+
+        Return a string representation of this CashRegister.
+
+        >>> reg1 = CashRegister(1, 2, 3, 4, 5)
+        >>> reg1.__str__()
+        'CashRegister: $160 ($1x1, $2x2, $5x3, $10x4, $20x5)'
+        """
+        return 'CashRegister: ${0} ($1x{1}, $2x{2}, $5x{3}, $10x{4}, $20x{5})'.format(self.get_total(),
+        self.loonies, self.toonies, self.fives, self.tens, self.twenties)
+
+
     def __eq__(self, other):
         """
         (CashRegister, CashRegister) -> bool
@@ -42,8 +61,12 @@ class CashRegister:
         (CashRegister) -> int
 
         Return the total amount of cash in the register.
-        """
 
+        >>> register = CashRegister(5, 5, 5, 5, 5)
+        >>> register.get_total()
+        190
+        >>>
+        """
         return self.loonies + self.toonies * 2 + self.fives * 5 + self.tens * 10 + self.twenties * 20
 
 
@@ -52,7 +75,15 @@ class CashRegister:
         (CashRegister, int, str) -> NoneType
 
         Add count items of denomination to the register.
-        denomination is one of 'loonies', 'toonies', 'fives', 'tens' and 'twenties'.
+        denomination is one of 'loonies', 'toonies', 'fives', 'tens', and 'twenties'.
+
+        >>> register = CashRegister(5, 5, 5, 5, 5)
+        >>> register.add(2, 'toonies')
+        >>> register.toonies
+        7
+        >>> register.add(1, 'tens')
+        >>> register.tens
+        6
         """
         if denomination == 'loonies':
             self.loonies += count
@@ -70,9 +101,18 @@ class CashRegister:
         """
         (CashRegister, int, str) -> NoneType
 
-        Remove count items of denomination from the register.
-        denomination is one of 'loonies', 'toonies', 'fives', 'tens', and 'twenties'
+        Remove count items of denomination from the register,
+        denomination is one of 'loonies', 'toonies', 'fives', 'tens', and 'twenties'.
+
+        >>> register = CashRegister(5, 5, 5, 5, 5)
+        >>> register.remove(2, 'toonies')
+        >>> register.toonies
+        3
+        >>> register.remove(1, 'tens')
+        >>> register.tens
+        4
         """
+        # This could also be done with a dictionary instead of if/elifs.
         if denomination == 'loonies':
             self.loonies -= count
         elif denomination == 'toonies':
@@ -85,11 +125,17 @@ class CashRegister:
             self.twenties -= count
 
 
-
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    cr0 = CashRegister(1, 2, 3, 4, 5)
     cr1 = CashRegister(2, 0, 0, 0, 0)
     cr2 = CashRegister(0, 1, 0, 0, 0)
     cr3 = CashRegister(1, 1, 0, 0, 0)
+    print()
+    print("cr0: ")
+    print(cr0)
     print()
     print("cr1: ")
     print(cr1)
@@ -103,6 +149,6 @@ if __name__ == '__main__':
     print("cr1 == cr2: ")
     print(cr1 == cr2)
     print()
-    print("cr2 == cr3: ")
+    print("cr3 == cr2: ")
     print(cr3 == cr2)
     print()
